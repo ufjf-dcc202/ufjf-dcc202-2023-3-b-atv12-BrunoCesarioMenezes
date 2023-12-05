a85df5791479eacd752ef8c7bc168aad532e55e6
BrunoCesarioMenezes <bruno.cesario@estudante.ufjf.br>
2023-12-05 20:50:06 -0300

    cria funçao leFormulario

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
 const olJoao = document.querySelector("ol#joao");
 const olMaria = document.querySelector("ol#maria");
 
+function leFormulario(event){
+
+    event.preventDefault();
+    const quantidade = document.entrada.quantidade.value;
+    valueAsNumber;
+    const fruta = document.entrada.fruta.value;
+    const origin = document.entrada.origem.value;
+    const destino = document.entrada.destino.value;
+
+    console.log(`${origem} doa ${quantidade} ${fruta} para ${destino}`);
+}
