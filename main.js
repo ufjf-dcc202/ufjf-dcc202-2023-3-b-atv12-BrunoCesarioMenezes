0918aacd45293db8c84648267c41508a633ae422
BrunoCesarioMenezes <bruno.cesario@estudante.ufjf.br>
2023-12-05 21:42:12 -0300

    cria função atualizaTela()

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
+import {getEstoque} from "./estoque.js";
+
 const olJoao = document.querySelector("ol#joao");
 const olMaria = document.querySelector("ol#maria");
 
 document.entrada.addEventListener('submit', leFormulario);
+
+atualizaTela();
 
 function leFormulario(event){
 
@@ -13,4 +17,15 @@ function leFormulario(event){
     const destino = document.entrada.destino.value;
 
     console.log(`${origem} doa ${quantidade} ${fruta} para ${destino}`);
+}
+
+function atualizaTela(){
+    const estoque = getEstoque();
+    olJoao.innerHTML = "";
+    for (let i=0; i<estoque.joao.length; i++){
+        const monte = estoque.joao[i];
+        const li = document.createElement('li');
+        li.textContent=`${monte.tipo}: ${monte.qtd}`;
+        olJoao.append(li);
+    }
 }
