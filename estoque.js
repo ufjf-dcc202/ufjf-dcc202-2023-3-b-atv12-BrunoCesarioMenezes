dc4b86db22194bc11cd414bda9b9c7ab17d3e87f
BrunoCesarioMenezes <bruno.cesario@estudante.ufjf.br>
2023-12-05 22:17:28 -0300

    corrige codigo

--- estoque.js
+++ estoque.js
@@ -19,7 +19,7 @@ function transacao(origem, destino, tipo, quantidade){
         for (let i=0; i<estoque[origem].length; i++){
             const monte = pessoa [i];
             if(monte.tipo === tipo){
-                monte.quantidade -= quantidade;
+                monte.qtd -= quantidade;
             }
         }
     }
